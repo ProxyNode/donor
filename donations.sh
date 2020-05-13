d29cdb51b319b51b1108088911afae390c08c7c5
ProxyNode <44655174+ProxyNode@users.noreply.github.com>
2020-05-13 23:52:33 +0100

    Create donations.sh

---
+++ donations.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+BLUE='\033[0;96m'
+GREEN='\033[0;92m'
+RED='\033[0;91m'
+YELLOW='\033[0;93m'
+CLEAR='\033[0m'
+clear
+echo
+echo
+echo
+echo -e "Donations can be made to multiple addresses for multiple projects"
+echo
+echo -e ${BLUE}" Your patronage is apprappreciated, tipping addresses"${CLEAR}
+echo
+echo -e ${YELLOW}" BTC address: bc1quff0mzm2wjdnh40whreudhv8mdk63rr0th678h"${CLEAR}
+echo -e ${WHITE}" LTC address: MTzhTEs2KuwQucneFAVSeCcmJ7WgffFF6B"${CLEAR}
+echo -e ${BLUE}" PRX address: PEDEmXCePvutr7UhZTb9dXYuCEg8j7o8Nv"${CLEAR}
+echo -e ${GREEN}" DOGE address: D9ken6LVNzLDQBKaiRH8v6inNNVQGyZyNo"${CLEAR}
+echo
+echo -e ${RED}"Need help?  Find sizzlephizzle#0376 on Discord - https://discord.gg/M2h8dCU"${CLEAR}
